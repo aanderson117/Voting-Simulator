493c9ef54a1b225547cfcd2c56cb32c7fa61d880
aanderson117 <handerson5070@gmail.com>
2022-12-09 21:07:10 -0600

    Fixed major issues, developed further

--- controller.py
+++ controller.py
@@ -1,40 +1,79 @@
 from PyQt5.QtWidgets import *
-from screen1 import *
-from screen2 import *
-from screen3 import *
+from view1 import *
 
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
 QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
 
-class Controller(QMainWindow, Ui_Window_votemenu, Ui_Window_candmenu, Ui_Window_resultmenu):
+class Controller(QMainWindow, Ui_Window_votemenu):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.setupUi(self)
         self.votes_john=0
         self.votes_jane=0
+        self.frame_votemenu.show()
+        self.frame_cand_menu.hide()
+        self.frame_results.hide()
+        self.button_restart.hide()
         self.button_vote.clicked.connect(lambda: self.vote())
-
-    def screen2(self):
-        self.window_candmenu = QtWidgets.QMainWindow()
-        self.ui = Ui_Window_candmenu()
-        self.ui.setupUi(self.window_candmenu)
-        self.window_candmenu.show()
-        ###FIXME: close vote menu when opening candidate menu###
-
-    def screen3(self):
-        self.window_resultmenu = QtWidgets.QMainWindow()
-        self.ui = Ui_Window_resultmenu()
-        self.ui.setupUi(self.window_resultmenu)
-        self.window_resultmenu.show()
-        ###FIXME: close cand menu when opening result menu###
+        self.button_submit.clicked.connect(lambda: self.choice())
+        self.button_voteagain.clicked.connect(lambda: self.revote())
+        self.button_viewresults.clicked.connect(lambda: self.results())
+        self.button_restart.clicked.connect(lambda: self.restart())
 
     def vote(self):
-        self.screen2()
+        self.frame_cand_menu.show()
+        self.frame_votemenu.hide()
 
     def choice(self):
         if self.radio_opt1.isChecked():
             self.votes_john += 1
+            self.frame_results.show()
+            self.frame_cand_menu.hide()
+            self.label_votedname.setText('Voted John')
         elif self.radio_opt2.isChecked():
             self.votes_jane += 1
+            self.frame_results.show()
+            self.frame_cand_menu.hide()
+            self.label_votedname.setText('Voted Jane')
+            self.radio_opt2.nextCheckState()
+        else:
+            self.label_candmenu.setText('Please select a candidate')
+
+    def revote(self):
+        self.label_candmenu.setText('Candidate Menu')
+        if self.radio_opt1.isChecked():
+            self.radio_opt1.nextCheckState()
+        if self.radio_opt2.isChecked():
+            self.radio_opt2.nextCheckState()
+        self.frame_cand_menu.show()
+        self.frame_results.hide()
+
+    def results(self):
+        self.label_votedname.setText('')
+        self.button_voteagain.hide()
+        self.button_viewresults.hide()
+        self.label_results.setGeometry(QtCore.QRect(100, 60, 301, 200))
+        self.label_results.setText(f'John Smith = {self.votes_john}\nJane Reynolds = {self.votes_jane}')
+        self.button_restart.show()
+
+    def restart(self):
+        self.frame_votemenu.show()
+        self.frame_results.hide()
+        self.button_restart.hide()
+        self.button_voteagain.show()
+        self.button_viewresults.show()
+        self.label_results.setGeometry(QtCore.QRect(100, 60, 301, 31))
+        self.label_results.setText('')
+        self.label_votedname.setText('')
+        if self.radio_opt1.isChecked():
+            self.radio_opt1.nextCheckState()
+        if self.radio_opt2.isChecked():
+            self.radio_opt2.nextCheckState()
+        self.votes_john = 0
+        self.votes_jane = 0
+
+
+
+
 
 
